f0213e1c81c4b885262c4ade8e8fd2e55315f642
Yomel Barreto <jairbarretoflores138@gmail.com>
2026-02-12 03:02:07 -0500

    Fix import error in store_index.py

--- store_index.py
+++ store_index.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from src.helper import load_pdf_file, text_split, download_embeddings
+from src.helper import load_pdf_file, text_split, download_embeddings, filter_to_minimal_docs
 from pinecone import Pinecone
 from pinecone import ServerlessSpec 
 from langchain_pinecone import PineconeVectorStore
